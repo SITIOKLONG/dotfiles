7e6f1bb934c3f764380bd420cd65560de9c08d6e
Jack <ilsit@connect.ust.hk>
2026-03-18 02:48:25 +0800

    new theme

--- README.md
+++ README.md
@@ -17,4 +17,44 @@ mpv video.mp4
 ssh -L 6006:localhost:6006 <hostname@ip>
 ```
 
+# nerdfront
+```shell
+brew install wget
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/0xProto.zip
+```
 
+# conda 
+```shell
+conda config --set auto_activate false
+```
+
+# ssh
+
+install ssh
+
+```bash
+sudo apt update
+sudo apt install openssh-server
+```
+
+check ssh service
+
+```bash
+sudo systemctl status ssh
+```
+
+allow ssh service
+
+```
+sudo ufw allow ssh
+sudo ufw enable
+```
+
+# Zerotier
+
+```
+sudo apt install openssh-server   # Ubuntu/Debian
+sudo systemctl enable --now ssh
+sudo ufw status
+sudo ufw allow 22
+```
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,8 @@
 -- Add any additional options here
 
 vim.opt.clipboard = "unnamedplus"
+vim.cmd("let g:netrw_liststyle = 3")
+vim.cmd('colorscheme catppuccin-mocha')
 
 vim.g.autoformat = false
 -- vim.g.neovide_cursor_vfx_mode = "pixiedust"
@@ -11,13 +13,13 @@ vim.g.autoformat = false
 vim.g.neovide_floating_shadow = false
 vim.g.neovide_input_ime = true
 vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_opacity = 0.4
+vim.g.neovide_opacity = 0.6
 vim.g.neovide_normal_opacity = 1.0
 vim.g.neovide_show_border = false
 vim.o.guifont = "Fira Code:h12"
 vim.g.neovide_window_blurred = true
-vim.g.neovide_floating_blur_amount_x = 16.0
-vim.g.neovide_floating_blur_amount_y = 16.0
+vim.g.neovide_floating_blur_amount_x = 4.0
+vim.g.neovide_floating_blur_amount_y = 4.0
 vim.g.neovide_floating_blur = 1
 
 vim.g.neovide_floating_shadow = true
